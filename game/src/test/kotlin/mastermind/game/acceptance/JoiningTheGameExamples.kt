9920d955269dbf9b23f441f1251ff5cd65f9b65f
Jakub Zalas <jakub@zalas.pl>
2023-08-09 20:19:54 +0100

    Make a request for the decoding board view

--- game/src/test/kotlin/mastermind/game/acceptance/JoiningTheGameExamples.kt
+++ game/src/test/kotlin/mastermind/game/acceptance/JoiningTheGameExamples.kt
@@ -1,10 +1,18 @@
 package mastermind.game.acceptance
 
 import org.http4k.client.ApacheClient
+import org.http4k.core.Body
+import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.CREATED
+import org.http4k.core.Status.Companion.OK
+import org.http4k.core.with
+import org.http4k.format.Jackson.auto
+import org.http4k.lens.Path
+import org.http4k.routing.bind
+import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -34,12 +42,17 @@ class JoiningTheGameExamples {
     }
 }
 
-val server =
-    { _: Request ->
+val server = routes(
+    "/games" bind POST to { _: Request ->
         Response(CREATED).header("Location", "/games/6e252c79-4d02-4b05-92ac-6040e8c7f057")
+    },
+    "/games/{id}" bind GET to { request ->
+        val id = Path.of("id")(request)
+        Response(OK).with(
+            Body.auto<DecodingBoard>().toLens() of DecodingBoard(id, 4, 12, emptyList(), "In progress")
+        )
     }
-        .asServer(Undertow(0))
-        .start()
+).asServer(Undertow(0)).start()
 val client = ApacheClient()
 
 data class Code(val pegs: List<String>) : List<String> by pegs {
@@ -55,9 +68,7 @@ data class DecodingBoard(
     val totalAttempts: Int,
     val guesses: List<Any>,
     val outcome: String
-) {
-
-}
+)
 
 private fun startApplication(totalAttempts: Int, secret: Code) {
 }
@@ -71,13 +82,11 @@ private fun joinGame(block: (GameId) -> Unit) {
         ?.also(block) ?: fail("Location header not found in the response.")
 }
 
-private fun viewDecodingBoard(gameId: GameId): DecodingBoard? = DecodingBoard(
-    gameId.value,
-    4,
-    12,
-    emptyList(),
-    "In progress"
-)
+private fun viewDecodingBoard(gameId: GameId): DecodingBoard {
+    val response = client(Request(GET, "http://localhost:${server.port()}/games/${gameId.value}"))
+    assertEquals(OK, response.status)
+    return Body.auto<DecodingBoard>().toLens()(response)
+}
 
 infix fun <T> T?.shouldBe(expected: T?) {
     assertEquals(expected, this)
