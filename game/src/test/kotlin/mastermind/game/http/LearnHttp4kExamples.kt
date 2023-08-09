43744498214b37b864b709091b86ae1211dcf49e
Jakub Zalas <jakub@zalas.pl>
2023-08-09 19:50:34 +0100

    Learn how to define http4k routes

--- game/src/test/kotlin/mastermind/game/http/LearnHttp4kExamples.kt
+++ game/src/test/kotlin/mastermind/game/http/LearnHttp4kExamples.kt
@@ -1,9 +1,16 @@
 package mastermind.game.http
 
 import org.http4k.client.ApacheClient
-import org.http4k.core.*
+import org.http4k.core.Body
+import org.http4k.core.ContentType
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.http4k.lens.string
+import org.http4k.routing.bind
+import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 import org.junit.jupiter.api.AfterEach
@@ -17,9 +24,11 @@ import org.junit.jupiter.api.Test
  */
 class LearnHttp4kExamples {
     private val client = ApacheClient()
-    private val server = { request: Request ->
-        Response(OK).body("Hello, ${request.query("name") ?: "anonymous"}!")
-    }.asServer(Undertow(0))
+    private val server = routes(
+        "/hello" bind GET to { request ->
+            Response(OK).body("Hello, ${request.query("name") ?: "anonymous"}!")
+        }
+    ).asServer(Undertow(0))
 
     @BeforeEach
     fun startServer() {
@@ -33,9 +42,16 @@ class LearnHttp4kExamples {
 
     @Test
     fun `client calls the server`() {
-        val response = client(Request(Method.GET, "http://localhost:${server.port()}/hello"))
+        val response = client(Request(GET, "http://localhost:${server.port()}/hello"))
 
         assertEquals(OK, response.status)
         assertEquals("Hello, anonymous!", Body.string(ContentType.TEXT_PLAIN).toLens()(response))
+    }
+
+    @Test
+    fun `client calls a route not found on the server`() {
+        val response = client(Request(GET, "http://localhost:${server.port()}/not-found"))
+
+        assertEquals(NOT_FOUND, response.status)
     }
 }
