f4c4d190b9e58040cee0d3c0801b60f9c9852687
Jakub Zalas <jakub@zalas.pl>
2023-08-09 11:39:33 +0100

    Configure JUnit and confirm it runs tests

--- game/build.gradle.kts
+++ game/build.gradle.kts
@@ -9,3 +9,16 @@ repositories {
 kotlin {
     jvmToolchain(19)
 }
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+}
---
+++ game/src/test/kotlin/mastermind/game/Tests.kt
@@ -0,0 +1,11 @@
+package mastermind.game
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Tests {
+    @Test
+    fun `it runs tests`() {
+        Assertions.assertEquals("", "")
+    }
+}
