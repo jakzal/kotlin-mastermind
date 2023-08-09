aaa4f96defa7abdef93241e10ed3991940836def
Jakub Zalas <jakub@zalas.pl>
2023-08-09 14:11:42 +0100

    Draft steps for the "Joining the game" scenario

---
+++ game/src/test/kotlin/mastermind/game/acceptance/JoiningTheGameExamples.kt
@@ -0,0 +1,14 @@
+package mastermind.game.acceptance
+
+import org.junit.jupiter.api.Test
+
+class JoiningTheGameExamples {
+    @Test
+    fun `code breaker joins the game`() {
+        // Given a decoding board of 12 attempts
+        // And the code maker has placed a secret on the board
+        // When I join the game
+        // Then the game should be started with an empty board
+        // And I should have 12 attempts available
+    }
+}
