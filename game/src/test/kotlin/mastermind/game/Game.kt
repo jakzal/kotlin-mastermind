b44ced7b1fdfa0bf0de52290eb6d1f22b3d44a2e
Jakub Zalas <jakub@zalas.pl>
2023-08-11 18:28:22 +0100

    Make GameId a property of every game command and game event

--- game/src/test/kotlin/mastermind/game/Game.kt
+++ game/src/test/kotlin/mastermind/game/Game.kt
@@ -1,9 +1,13 @@
 package mastermind.game
 
-sealed interface GameCommand
-data class JoinGame(val gameId: GameId, val secret: Code, val totalAttempts: Int) : GameCommand
+sealed interface GameCommand {
+    val gameId: GameId
+}
+data class JoinGame(override val gameId: GameId, val secret: Code, val totalAttempts: Int) : GameCommand
 
-sealed interface GameEvent
-data class GameStarted(val gameId: GameId, val secret: Code, val totalAttempts: Int) : GameEvent
+sealed interface GameEvent {
+    val gameId: GameId
+}
+data class GameStarted(override val gameId: GameId, val secret: Code, val totalAttempts: Int) : GameEvent
 
 sealed interface GameFailure
