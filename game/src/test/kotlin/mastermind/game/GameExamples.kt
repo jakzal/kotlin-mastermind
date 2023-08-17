0a16a7ebda7260b1873cef4662395ba343a54898
Jakub Zalas <jakub@zalas.pl>
2023-08-17 12:23:56 +0200

    Refactor the test case to parametrized test

--- game/src/test/kotlin/mastermind/game/GameExamples.kt
+++ game/src/test/kotlin/mastermind/game/GameExamples.kt
@@ -4,6 +4,9 @@ import arrow.core.nonEmptyListOf
 import mastermind.game.testkit.anyGameId
 import mastermind.game.testkit.shouldSucceedWith
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class GameExamples {
     private val gameId = anyGameId()
@@ -36,18 +39,23 @@ class GameExamples {
         )
     }
 
-    @Test
-    fun `it gives black peg for each code peg on the correct position`() {
-        val secret = Code("Red", "Green", "Blue", "Yellow")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("guessExamples")
+    fun `it gives feedback on the guess`(message: String, secret: Code, guess: Code, feedback: Feedback) {
+        val secret = secret
         val game = nonEmptyListOf(GameStarted(gameId, secret, totalAttempts))
 
-        execute(MakeGuess(gameId, Code("Red", "Purple", "Blue", "Purple")), game) shouldSucceedWith listOf(
-            GuessMade(
-                gameId,
-                Guess(
-                    Code("Red", "Purple", "Blue", "Purple"),
-                    Feedback(listOf("Black", "Black"), Feedback.Outcome.IN_PROGRESS)
-                )
+        execute(MakeGuess(gameId, guess), game) shouldSucceedWith listOf(GuessMade(gameId, Guess(guess, feedback)))
+    }
+
+    companion object {
+        @JvmStatic
+        fun guessExamples(): List<Arguments> = listOf(
+            Arguments.of(
+                "it gives black peg for each code peg on the correct position",
+                Code("Red", "Green", "Blue", "Yellow"),
+                Code("Red", "Purple", "Blue", "Purple"),
+                Feedback(listOf("Black", "Black"), Feedback.Outcome.IN_PROGRESS)
             )
         )
     }
