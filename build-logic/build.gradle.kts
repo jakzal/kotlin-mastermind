3cf5ce51962ff0e4aeb8fde93ff892b8788e6dc1
Jakub Zalas <jakub@zalas.pl>
2023-09-01 09:56:39 +0100

    Update Kotlin (1.9.0 -> 1.9.10)

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-    val kotlinVersion = "1.9.0"
+    val kotlinVersion = "1.9.10"
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 }
