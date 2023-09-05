99f794074fd11d06d0cbeb5dd537e19bb5dac310
Jakub Zalas <jakub@zalas.pl>
2023-09-05 21:42:56 +0100

    Revert "Configure pitest"
    This reverts commit fce31cc6d598fa6bdd3e055e236e998cd37eda71.

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,5 +9,4 @@ repositories {
 dependencies {
     val kotlinVersion = "1.9.10"
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.9.11")
 }
