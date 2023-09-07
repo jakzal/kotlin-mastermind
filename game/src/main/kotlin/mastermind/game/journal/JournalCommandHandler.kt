395ec77aee987b7de5e6f5d3656e075a695b8d61
Jakub Zalas <jakub@zalas.pl>
2023-09-07 11:24:16 +0100

    Fold events to calculate the state

--- game/src/main/kotlin/mastermind/game/journal/JournalCommandHandler.kt
+++ game/src/main/kotlin/mastermind/game/journal/JournalCommandHandler.kt
@@ -2,27 +2,27 @@ package mastermind.game.journal
 
 import arrow.core.Either
 import arrow.core.NonEmptyList
-import arrow.core.toNonEmptyListOrNull
 import mastermind.game.CommandHandler
 import mastermind.journal.Journal
 import mastermind.journal.JournalFailure
 import mastermind.journal.append
 
+// @TODO sort out order of type parameters
+
 typealias Execute<COMMAND, EVENT, STATE, FAILURE> = (COMMAND, STATE?) -> Either<FAILURE, NonEmptyList<EVENT>>
 typealias Apply<STATE, EVENT> = (STATE?, EVENT) -> STATE
 
 context(Journal<EVENT, FAILURE>)
-class JournalCommandHandler<COMMAND : Any, EVENT : Any, FAILURE : Any, RESULT>(
-    // @TODO use applyEvent to reconstitute state
-    private val applyEvent: Apply<NonEmptyList<EVENT>, EVENT>,
-    private val execute: Execute<COMMAND, EVENT, NonEmptyList<EVENT>, FAILURE>,
+class JournalCommandHandler<COMMAND : Any, EVENT : Any, FAILURE : Any, RESULT, STATE : Any>(
+    private val applyEvent: Apply<STATE, EVENT>,
+    private val execute: Execute<COMMAND, EVENT, STATE, FAILURE>,
     private val streamNameResolver: (COMMAND) -> String,
     private val calculateResult: (NonEmptyList<EVENT>) -> RESULT
 ) : CommandHandler<COMMAND, JournalFailure<FAILURE>, RESULT> {
     override suspend operator fun invoke(command: COMMAND): Either<JournalFailure<FAILURE>, RESULT> {
         return stream(streamNameResolver(command)) {
             append {
-                execute(command, this.events.toNonEmptyListOrNull())
+                execute(command, events.fold(null, applyEvent))
             }
         }.map {
             calculateResult(it.events)
--- game/src/test/kotlin/mastermind/game/journal/JournalCommandHandlerExamples.kt
+++ game/src/test/kotlin/mastermind/game/journal/JournalCommandHandlerExamples.kt
@@ -1,14 +1,17 @@
 package mastermind.game.journal
 
 import arrow.core.NonEmptyList
+import arrow.core.left
 import arrow.core.nonEmptyListOf
 import arrow.core.raise.either
 import arrow.core.right
 import kotlinx.coroutines.test.runTest
 import mastermind.game.testkit.shouldBe
+import mastermind.game.testkit.shouldFailWith
 import mastermind.game.testkit.shouldReturn
 import mastermind.game.testkit.shouldSucceedWith
 import mastermind.journal.InMemoryJournal
+import mastermind.journal.JournalFailure.ExecutionFailure
 import mastermind.journal.Stream.LoadedStream
 import mastermind.journal.append
 import org.junit.jupiter.api.Test
@@ -57,10 +60,47 @@ class JournalCommandHandlerExamples {
             JournalCommandHandler(apply, execute, streamNameResolver) { events -> events.map { it.id }.joinToString(",") }
         }
 
-        handler(TestCommand("ABC")) shouldReturn "123,456,ABC".right()
+        handler(TestCommand("ABC")) shouldSucceedWith  "123,456,ABC"
+    }
+
+    @Test
+    fun `it reconstitutes any state from the loaded list of events`() = runTest {
+        val applyEvent: Apply<TestState, TestEvent> = { state, event ->
+            state?.let { TestState(it.history + event.id) } ?: TestState(listOf(event.id))
+        }
+        val execute: Execute<TestCommand, TestEvent, TestState, TestFailure> =
+            { _: TestCommand, state: TestState? ->
+                either {
+                    nonEmptyListOf(expectedEvent).also {
+                        state shouldBe TestState(listOf("987", "654"))
+                    }
+                }
+            }
+
+        journal.stream("Stream:ABC") {
+            append(TestEvent("987"), TestEvent("654"))
+        }
+
+        val handler = with(journal) {
+            JournalCommandHandler(applyEvent, execute, streamNameResolver) { events -> events }
+        }
+
+        handler(TestCommand("ABC")) shouldSucceedWith listOf(TestEvent("987"), TestEvent("654"), expectedEvent)
+    }
+
+    @Test
+    fun `it returns journal failure in case execute fails`() = runTest {
+        val execute: Execute<TestCommand, TestEvent, NonEmptyList<TestEvent>, TestFailure> =
+            { _: TestCommand, _: NonEmptyList<TestEvent>? -> TestFailure("Execution failed.").left() }
+        val handler = with(journal) {
+            JournalCommandHandler(apply, execute, streamNameResolver) { events -> events.head.id }
+        }
+
+        handler(TestCommand("ABC")) shouldFailWith ExecutionFailure(TestFailure("Execution failed."))
     }
 
     private data class TestCommand(val id: String)
     private data class TestEvent(val id: String)
     private data class TestFailure(val cause: String)
+    private data class TestState(val history: List<String>)
 }
