899bb941e9601a04c4882e3dad06bf8547a95ed1
Jakub Zalas <jakub@zalas.pl>
2023-09-07 21:51:46 +0100

    Learn how to write to and read from the eventstoredb

--- lib/journal-eventstoredb/build.gradle.kts
+++ lib/journal-eventstoredb/build.gradle.kts
@@ -5,4 +5,8 @@ plugins {
 dependencies {
     implementation(project(":lib:journal"))
     implementation("com.eventstore:db-client-java:4.2.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.+")
+    testImplementation(project(":lib:testkit:testcontainers-eventstoredb"))
+    testImplementation("org.testcontainers:testcontainers:1.19.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.0")
 }
