66b2e6d2515b1845c6294e300e8dcef5236b6dce
Jakub Zalas <jakub@zalas.pl>
2023-09-13 10:39:30 +0100

    Use jackson to write the object to an array of bytes

--- lib/journal-eventstoredb/src/test/kotlin/mastermind/journal/eventstoredb/serialization/SerializationExamples.kt
+++ lib/journal-eventstoredb/src/test/kotlin/mastermind/journal/eventstoredb/serialization/SerializationExamples.kt
@@ -1,5 +1,6 @@
 package mastermind.journal.eventstoredb.serialization
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import mastermind.journal.eventstoredb.serialization.SerializationExamples.TestEvent.Event1
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -8,13 +9,14 @@ class SerializationExamples {
 
     @Test
     fun `it writes an object to an array of bytes`() {
-        val writer: (TestEvent) -> ByteArray = { _ -> """{"name":"First event"}""".toByteArray() }
+        val writer: (TestEvent) -> ByteArray = { event -> jacksonObjectMapper().writeValueAsBytes(event) }
 
         writer(Event1("First event")) shouldReturnJson """{"name":"First event"}"""
     }
 
     sealed interface TestEvent {
         data class Event1(val name: String) : TestEvent
+        data class Event2(val id: Int, val name: String) : TestEvent
     }
 }
 
