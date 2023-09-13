5849e6495228c77ef21d6d4347622c3349740c49
Jakub Zalas <jakub@zalas.pl>
2023-09-13 20:17:13 +0100

    Make a note of missing expected and actual versions

--- lib/journal/src/main/kotlin/mastermind/journal/JournalFailure.kt
+++ lib/journal/src/main/kotlin/mastermind/journal/JournalFailure.kt
@@ -3,6 +3,7 @@ package mastermind.journal
 sealed interface JournalFailure<FAILURE> {
     sealed interface EventStoreFailure<FAILURE> : JournalFailure<FAILURE> {
         data class StreamNotFound<FAILURE>(val streamName: StreamName) : EventStoreFailure<FAILURE>
+        // @TODO include the expected and actual versions
         data class VersionConflict<FAILURE>(val streamName: StreamName) : EventStoreFailure<FAILURE>
     }
 
