7cbab6c131cf6c0a60f2d94048802498048b2780
Jakub Zalas <jakub@zalas.pl>
2023-09-13 11:32:17 +0100

    Handle read failures

--- lib/journal-eventstoredb/src/main/kotlin/mastermind/journal/eventstoredb/serialization.kt
+++ lib/journal-eventstoredb/src/main/kotlin/mastermind/journal/eventstoredb/serialization.kt
@@ -1,12 +1,22 @@
 package mastermind.journal.eventstoredb
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlin.reflect.KClass
 
+data class ReadFailure(val cause: Throwable)
 
 fun <T : Any> createWriter(objectMapper: ObjectMapper = jacksonObjectMapper()): T.() -> ByteArray =
     objectMapper::writeValueAsBytes
 
-fun <T : Any> createReader(objectMapper: ObjectMapper = jacksonObjectMapper()): ByteArray.(KClass<out T>) -> T =
-    { type -> objectMapper.readValue(this, type.java) }
+fun <T : Any> createReader(objectMapper: ObjectMapper = jacksonObjectMapper()): ByteArray.(KClass<out T>) -> Either<ReadFailure, T> =
+    { type ->
+        try {
+            objectMapper.readValue(this, type.java).right()
+        } catch (cause: Throwable) {
+            ReadFailure(cause).left()
+        }
+    }
--- lib/journal-eventstoredb/src/test/kotlin/mastermind/journal/eventstoredb/SerializationExamples.kt
+++ lib/journal-eventstoredb/src/test/kotlin/mastermind/journal/eventstoredb/SerializationExamples.kt
@@ -1,8 +1,12 @@
 package mastermind.journal.eventstoredb
 
+import arrow.core.Either
+import arrow.core.right
+import com.fasterxml.jackson.core.io.JsonEOFException
 import mastermind.journal.eventstoredb.SerializationExamples.TestEvent.Event1
 import mastermind.journal.eventstoredb.SerializationExamples.TestEvent.Event2
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 
 class SerializationExamples {
@@ -19,7 +23,17 @@ class SerializationExamples {
         val asObject = createReader<TestEvent>()
         val bytes = """{"id":13, "name":"Second event"}""".toByteArray()
 
-        bytes.asObject(Event2::class) shouldReturn Event2(13, "Second event")
+        bytes.asObject(Event2::class) shouldSucceedWith Event2(13, "Second event")
+    }
+
+    @Test
+    fun `it returns an error if the object cannot be read`() {
+        val asObject = createReader<TestEvent>()
+        val malformedBytes = """{"id":13, "name":"Second event"""".toByteArray()
+
+        malformedBytes.asObject(Event2::class) shouldFailWith { failure: ReadFailure ->
+            failure.cause shouldHaveTypeOf JsonEOFException::class.java
+        }
     }
 
     sealed interface TestEvent {
@@ -28,10 +42,20 @@ class SerializationExamples {
     }
 }
 
-infix fun ByteArray.shouldReturnJson(expected: String) {
+private infix fun ByteArray.shouldReturnJson(expected: String) {
     assertEquals(expected, this.decodeToString(), "`$expected` is `${this.decodeToString()}`")
 }
 
-infix fun <T> T.shouldReturn(expected: T) {
-    assertEquals(expected, this, "`$expected` is `$this`")
+private infix fun <T> T.shouldSucceedWith(expected: T) {
+    assertEquals(expected.right(), this, "`$expected` is `$this`")
+}
+
+private infix fun <A, B> Either<A, B>.shouldFailWith(onFailure: (A) -> Unit) {
+    this
+        .onRight { fail("Expected a failure but got: `$this`.") }
+        .mapLeft(onFailure)
+}
+
+private infix fun Any.shouldHaveTypeOf(type: Class<out Throwable>) {
+    assertEquals(type, this::class.java)
 }
