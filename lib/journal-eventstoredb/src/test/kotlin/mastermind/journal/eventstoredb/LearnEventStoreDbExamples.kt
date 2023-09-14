6bcac161cfa516c783776bd4f1c40a85873f687a
Jakub Zalas <jakub@zalas.pl>
2023-09-14 11:52:42 +0100

    Add the missing learning tag

--- lib/journal-eventstoredb/src/test/kotlin/mastermind/journal/eventstoredb/LearnEventStoreDbExamples.kt
+++ lib/journal-eventstoredb/src/test/kotlin/mastermind/journal/eventstoredb/LearnEventStoreDbExamples.kt
@@ -14,6 +14,7 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers(disabledWithoutDocker = true)
+@Tag("learning")
 @Tag("io")
 class LearnEventStoreDbExamples {
     companion object {
