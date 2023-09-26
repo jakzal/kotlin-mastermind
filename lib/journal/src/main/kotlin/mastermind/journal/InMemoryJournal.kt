c6f27f359360d9f8705d621e1b540dd6283a0132
Jakub Zalas <jakub@zalas.pl>
2023-09-26 10:40:59 +0100

    Extract an in-memory event store from the journal

--- lib/journal/src/main/kotlin/mastermind/journal/InMemoryJournal.kt
+++ lib/journal/src/main/kotlin/mastermind/journal/InMemoryJournal.kt
@@ -9,31 +9,34 @@ import mastermind.journal.JournalFailure.EventStoreFailure.VersionConflict
 import mastermind.journal.JournalFailure.ExecutionFailure
 import mastermind.journal.Stream.*
 
-class InMemoryJournal<EVENT : Any, FAILURE : Any> : Journal<EVENT, FAILURE> {
+class InMemoryEventStore<EVENT : Any, FAILURE : Any> {
     private val events = Atomic(mapOf<StreamName, LoadedStream<EVENT>>())
-    val loadStream: (StreamName) -> Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>> =
-        { streamName: StreamName ->
-            events.get()[streamName]?.right() ?: StreamNotFound<FAILURE>(streamName).left()
-        }
-    val append: (UpdatedStream<EVENT>) -> Either<JournalFailure<FAILURE>, LoadedStream<EVENT>> =
-        { streamToWrite ->
-            either {
-                events.updateAndGet {
-                    it[streamToWrite.streamName]?.let { writtenStream ->
-                        if (writtenStream.streamVersion != streamToWrite.streamVersion) {
-                            raise(
-                                VersionConflict(
-                                    streamToWrite.streamName,
-                                    streamToWrite.streamVersion,
-                                    writtenStream.streamVersion
-                                )
+
+    fun load(streamName: StreamName): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>> =
+        events.get()[streamName]?.right() ?: StreamNotFound<FAILURE>(streamName).left()
+
+    fun append(stream: UpdatedStream<EVENT>): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>> =
+        either {
+            events.updateAndGet {
+                it[stream.streamName]?.let { writtenStream ->
+                    if (writtenStream.streamVersion != stream.streamVersion) {
+                        raise(
+                            VersionConflict(
+                                stream.streamName,
+                                stream.streamVersion,
+                                writtenStream.streamVersion
                             )
-                        }
+                        )
                     }
-                    it + mapOf(streamToWrite.streamName to streamToWrite.toLoadedStream())
-                }[streamToWrite.streamName] ?: raise(StreamNotFound(streamToWrite.streamName))
-            }
+                }
+                it + mapOf(stream.streamName to stream.toLoadedStream())
+            }[stream.streamName] ?: raise(StreamNotFound(stream.streamName))
         }
+}
+
+class InMemoryJournal<EVENT : Any, FAILURE : Any>(
+    private val eventStore: InMemoryEventStore<EVENT, FAILURE> = InMemoryEventStore()
+) : Journal<EVENT, FAILURE> {
 
     override suspend fun stream(
         streamName: StreamName,
@@ -45,7 +48,7 @@ class InMemoryJournal<EVENT : Any, FAILURE : Any> : Journal<EVENT, FAILURE> {
             .append()
 
     override suspend fun load(streamName: StreamName): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>> =
-        loadStream(streamName)
+        (eventStore::load)(streamName)
 
     private fun Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>>.orCreate(streamName: StreamName): Either<JournalFailure<FAILURE>, Stream<EVENT>> =
         recover<JournalFailure<FAILURE>, JournalFailure<FAILURE>, Stream<EVENT>> { e ->
@@ -57,7 +60,7 @@ class InMemoryJournal<EVENT : Any, FAILURE : Any> : Journal<EVENT, FAILURE> {
         flatMap { stream -> stream.onStream().mapLeft(::ExecutionFailure) }
 
     private fun Either<JournalFailure<FAILURE>, UpdatedStream<EVENT>>.append(): Either<JournalFailure<FAILURE>, LoadedStream<EVENT>> =
-        flatMap { streamToWrite -> append(streamToWrite) }
+        flatMap { streamToWrite -> (eventStore::append)(streamToWrite) }
 }
 
 private fun <EVENT : Any> UpdatedStream<EVENT>.toLoadedStream(): LoadedStream<EVENT> =
