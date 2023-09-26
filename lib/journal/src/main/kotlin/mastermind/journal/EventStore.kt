2c8a12b97910f8dd1b595b6498dec92cc093e18b
Jakub Zalas <jakub@zalas.pl>
2023-09-26 13:52:09 +0100

    Add imports

--- lib/journal/src/main/kotlin/mastermind/journal/EventStore.kt
+++ lib/journal/src/main/kotlin/mastermind/journal/EventStore.kt
@@ -3,8 +3,9 @@ package mastermind.journal
 import arrow.core.Either
 import mastermind.journal.JournalFailure.EventStoreFailure
 import mastermind.journal.Stream.LoadedStream
+import mastermind.journal.Stream.UpdatedStream
 
 interface EventStore<EVENT : Any, FAILURE : Any> {
     suspend fun load(streamName: StreamName): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>>
-    suspend fun append(stream: Stream.UpdatedStream<EVENT>): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>>
+    suspend fun append(stream: UpdatedStream<EVENT>): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>>
 }
