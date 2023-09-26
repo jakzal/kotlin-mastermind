21dffca2080b633389226cea47fd5e2b0d9d3487
Jakub Zalas <jakub@zalas.pl>
2023-09-26 11:42:02 +0100

    Extract the in-memory eventstore

---
+++ lib/journal/src/main/kotlin/mastermind/journal/InMemoryEventStore.kt
@@ -0,0 +1,42 @@
+package mastermind.journal
+
+import arrow.atomic.Atomic
+import arrow.core.*
+import arrow.core.raise.either
+import mastermind.journal.JournalFailure.EventStoreFailure
+import mastermind.journal.JournalFailure.EventStoreFailure.StreamNotFound
+import mastermind.journal.Stream.LoadedStream
+import mastermind.journal.Stream.UpdatedStream
+
+class InMemoryEventStore<EVENT : Any, FAILURE : Any>(
+    private val events: Atomic<Map<StreamName, LoadedStream<EVENT>>> = Atomic(mapOf())
+) : EventStore<EVENT, FAILURE> {
+    override fun load(streamName: StreamName): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>> =
+        events.get()[streamName]?.right() ?: StreamNotFound<FAILURE>(streamName).left()
+
+    override fun append(stream: UpdatedStream<EVENT>): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>> =
+        either {
+            events.updateAndGet {
+                it[stream.streamName]?.let { writtenStream ->
+                    if (writtenStream.streamVersion != stream.streamVersion) {
+                        raise(
+                            EventStoreFailure.VersionConflict(
+                                stream.streamName,
+                                stream.streamVersion,
+                                writtenStream.streamVersion
+                            )
+                        )
+                    }
+                }
+                it + mapOf(stream.streamName to stream.toLoadedStream())
+            }[stream.streamName] ?: raise(StreamNotFound(stream.streamName))
+        }
+}
+
+private fun <EVENT : Any> UpdatedStream<EVENT>.toLoadedStream(): LoadedStream<EVENT> =
+    LoadedStream(streamName, streamVersion + eventsToAppend.size, events + eventsToAppend)
+
+private infix operator fun <T> List<T>.plus(other: NonEmptyList<T>): NonEmptyList<T> =
+    toNonEmptyListOrNone()
+        .map { items -> items + other }
+        .getOrElse { other }
--- lib/journal/src/main/kotlin/mastermind/journal/InMemoryJournal.kt
+++ lib/journal/src/main/kotlin/mastermind/journal/InMemoryJournal.kt
@@ -1,39 +1,13 @@
 package mastermind.journal
 
-import arrow.atomic.Atomic
-import arrow.core.*
-import arrow.core.raise.either
+import arrow.core.Either
+import arrow.core.flatMap
+import arrow.core.recover
 import mastermind.journal.JournalFailure.EventStoreFailure
 import mastermind.journal.JournalFailure.EventStoreFailure.StreamNotFound
-import mastermind.journal.JournalFailure.EventStoreFailure.VersionConflict
 import mastermind.journal.JournalFailure.ExecutionFailure
 import mastermind.journal.Stream.*
 
-
-class InMemoryEventStore<EVENT : Any, FAILURE : Any>(
-    private val events: Atomic<Map<StreamName, LoadedStream<EVENT>>> = Atomic(mapOf())
-) : EventStore<EVENT, FAILURE> {
-    override fun load(streamName: StreamName): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>> =
-        events.get()[streamName]?.right() ?: StreamNotFound<FAILURE>(streamName).left()
-
-    override fun append(stream: UpdatedStream<EVENT>): Either<EventStoreFailure<FAILURE>, LoadedStream<EVENT>> =
-        either {
-            events.updateAndGet {
-                it[stream.streamName]?.let { writtenStream ->
-                    if (writtenStream.streamVersion != stream.streamVersion) {
-                        raise(
-                            VersionConflict(
-                                stream.streamName,
-                                stream.streamVersion,
-                                writtenStream.streamVersion
-                            )
-                        )
-                    }
-                }
-                it + mapOf(stream.streamName to stream.toLoadedStream())
-            }[stream.streamName] ?: raise(StreamNotFound(stream.streamName))
-        }
-}
 
 class InMemoryJournal<EVENT : Any, FAILURE : Any>(
     private val eventStore: InMemoryEventStore<EVENT, FAILURE> = InMemoryEventStore()
@@ -64,10 +38,3 @@ class InMemoryJournal<EVENT : Any, FAILURE : Any>(
         flatMap { streamToWrite -> (eventStore::append)(streamToWrite) }
 }
 
-private fun <EVENT : Any> UpdatedStream<EVENT>.toLoadedStream(): LoadedStream<EVENT> =
-    LoadedStream(streamName, streamVersion + eventsToAppend.size, events + eventsToAppend)
-
-private infix operator fun <T> List<T>.plus(other: NonEmptyList<T>): NonEmptyList<T> =
-    toNonEmptyListOrNone()
-        .map { items -> items + other }
-        .getOrElse { other }
