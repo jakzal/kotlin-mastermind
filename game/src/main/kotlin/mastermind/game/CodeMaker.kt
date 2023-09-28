21a7e1ae536e031afd6a4cb2515db1c919fb594e
Jakub Zalas <jakub@zalas.pl>
2023-09-28 20:45:08 +0100

    Group generation functions together

--- game/src/main/kotlin/mastermind/game/CodeMaker.kt
+++
@@ -1,3 +0,0 @@
-package mastermind.game
-
-fun Set<Code.Peg>.makeCode(length: Int = 4) = Code((1..length).map { this.random() })
--- game/src/main/kotlin/mastermind/game/generation.kt
+++ game/src/main/kotlin/mastermind/game/generation.kt
@@ -2,6 +2,8 @@ package mastermind.game
 
 import java.util.*
 
+fun Set<Code.Peg>.makeCode(length: Int = 4) = Code((1..length).map { this.random() })
+
 fun generateGameId(): GameId {
     return GameId(UUID.randomUUID().toString())
 }
