f119abdf28ea6b8b7f0656773870ae7c3f212391
Jakub Zalas <jakub@zalas.pl>
2023-10-17 09:51:59 +0100

    Remove an unused handler

--- lib/event-sourcing/src/main/kotlin/mastermind/eventsourcing/handlers.kt
+++ lib/event-sourcing/src/main/kotlin/mastermind/eventsourcing/handlers.kt
@@ -1,13 +1,5 @@
 package mastermind.eventsourcing
 
-import arrow.core.NonEmptyList
-import arrow.core.toNonEmptyListOrNull
-
-fun <COMMAND : Any, EVENT : Any, ERROR : Any> handlerFor(
-    execute: Execute<COMMAND, NonEmptyList<EVENT>?, ERROR, EVENT>
-): Execute<COMMAND, List<EVENT>, ERROR, EVENT> =
-    { command, events -> execute(command, events.toNonEmptyListOrNull()) }
-
 fun <COMMAND : Any, EVENT : Any, ERROR : Any, STATE> handlerFor(
     execute: Execute<COMMAND, STATE, ERROR, EVENT>,
     applyEvent: Apply<STATE, EVENT>,
