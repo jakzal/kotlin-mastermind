5cbca62297fb3e871d773ecb1c7469984613a253
Jakub Zalas <jakub@zalas.pl>
2024-01-11 10:43:41 +0000

    Update eventstoredb client library

--- lib/eventstore-eventstoredb/build.gradle.kts
+++ lib/eventstore-eventstoredb/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":lib:eventstore"))
-    implementation("com.eventstore:db-client-java:4.2.0")
+    implementation("com.eventstore:db-client-java:5.2.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.+")
     testImplementation(testFixtures(project(":lib:eventstore")))
     testImplementation(project(":lib:testkit:testcontainers-eventstoredb"))
--- lib/testkit/testcontainers-eventstoredb/build.gradle.kts
+++ lib/testkit/testcontainers-eventstoredb/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     implementation(platform("org.testcontainers:testcontainers-bom:1.19.2"))
     implementation("org.testcontainers:testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("com.eventstore:db-client-java:4.3.0")
+    testImplementation("com.eventstore:db-client-java:5.2.0")
     testImplementation("org.slf4j:slf4j-api:2.0.9")
 }
