60146b81e60c31522ba81eae432bf1df5e1db861
Jakub Zalas <jakub@zalas.pl>
2024-08-08 11:31:51 +0100

    Update the jackson kotlin module (2.15 -> 2.17)

--- lib/eventstore-eventstoredb/build.gradle.kts
+++ lib/eventstore-eventstoredb/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation(project(":lib:eventstore"))
     implementation("com.eventstore:db-client-java:5.4.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.+")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.+")
     testImplementation(testFixtures(project(":lib:eventstore")))
     testImplementation(project(":lib:testkit:testcontainers-eventstoredb"))
     testImplementation(platform("org.testcontainers:testcontainers-bom:1.19.2"))
