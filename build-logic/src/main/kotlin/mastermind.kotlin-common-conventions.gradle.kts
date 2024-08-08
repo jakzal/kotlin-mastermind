84f74529a778f0159a81464940deeb735c2c916c
Jakub Zalas <jakub@zalas.pl>
2024-08-08 11:24:28 +0100

    Update junit (5.10.0 -> 5.10.3)

--- build-logic/src/main/kotlin/mastermind.kotlin-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/mastermind.kotlin-common-conventions.gradle.kts
@@ -21,13 +21,13 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:1.2.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     if (useTestFixtures) {
         testFixturesImplementation("io.arrow-kt:arrow-core:1.2.4")
         testFixturesImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
-        testFixturesImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testFixturesImplementation(platform("org.junit:junit-bom:5.10.3"))
         testFixturesImplementation("org.junit.jupiter:junit-jupiter")
     }
 }
