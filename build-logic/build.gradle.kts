21a42af9ce5083db3723f138a356d7981e2e19c3
Jakub Zalas <jakub@zalas.pl>
2025-03-11 22:06:58 +0000

    Update Gradle and Kotlin

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-    val kotlinVersion = "2.0.0"
+    val kotlinVersion = "2.1.10"
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 }
