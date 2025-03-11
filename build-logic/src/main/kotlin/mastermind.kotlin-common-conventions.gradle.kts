4f805e287a49c93f5fea0f7d8318f1f72d44e66d
Jakub Zalas <jakub@zalas.pl>
2025-03-11 22:14:25 +0000

    Update arrow-kt

--- build-logic/src/main/kotlin/mastermind.kotlin-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/mastermind.kotlin-common-conventions.gradle.kts
@@ -16,14 +16,14 @@ kotlin {
 }
 
 dependencies {
-    implementation("io.arrow-kt:arrow-core:1.2.4")
+    implementation("io.arrow-kt:arrow-core:2.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     if (useTestFixtures) {
-        testFixturesImplementation("io.arrow-kt:arrow-core:1.2.4")
+        testFixturesImplementation("io.arrow-kt:arrow-core:2.0.1")
         testFixturesImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
         testFixturesImplementation(platform("org.junit:junit-bom:5.10.3"))
         testFixturesImplementation("org.junit.jupiter:junit-jupiter")
