627a08025c569dd24fe8e1aa133d693572e3f2b0
Jakub Zalas <jakub@zalas.pl>
2025-03-11 22:19:23 +0000

    Update eventstore db client

--- lib/testkit/testcontainers-eventstoredb/build.gradle.kts
+++ lib/testkit/testcontainers-eventstoredb/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     implementation(platform("org.testcontainers:testcontainers-bom:1.20.1"))
     implementation("org.testcontainers:testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("com.eventstore:db-client-java:5.4.1")
+    testImplementation("com.eventstore:db-client-java:5.4.4")
     testImplementation("org.slf4j:slf4j-api:2.0.14")
 }
